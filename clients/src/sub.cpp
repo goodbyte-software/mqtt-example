5d44f587c0d6de58e66094fc622afb885c8f9d32
Kamil Siecinski <kamil.siecinski@goupgo.pl>
2024-07-01 21:17:10 +0200

    set ca.crt as trust store

--- clients/src/sub.cpp
+++ clients/src/sub.cpp
@@ -8,7 +8,7 @@ const std::string SERVER_ADDRESS    { "mqtts://localhost:8883" };
 const std::string CLIENT_ID         { "subscriber" };
 
 const std::string KEY_STORE         { "brokers/certs/ClientJohn.key" };
-const std::string TRUST_STORE       { "brokers/certs/ClientJohn.crt" };
+const std::string TRUST_STORE       { "brokers/certs/ca.crt" };
 
 int main(int argc, char* argv[])
 {
